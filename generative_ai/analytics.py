859154a426f0ede61866dd39b8b57b502204022d
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-11-23 21:38:06 +0530

    Add the analytics section

---
+++ generative_ai/analytics.py
@@ -0,0 +1,7 @@
+def calculate_performance_metrics():
+    metrics = {
+        "system_uptime": "99.95%",
+        "latency_reduction": "15%",
+        "error_rate": "0.02%"
+    }
+    return metrics
